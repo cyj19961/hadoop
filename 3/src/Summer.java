5273cd4f624a9e468946fb11420a4a36608fa965
cyj19961 <73214933+cyj19961@users.noreply.github.com>
2024-04-14 20:22:11 +0300

    Create Summer.java

---
+++ 3/src/Summer.java
@@ -0,0 +1,40 @@
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.*;
+
+import java.io.*;
+import java.util.*;
+
+
+
+public class Summer
+extends Reducer<Text, Text, Text, Text>
+{
+    public void reduce(Text key, Iterable<Text> values, Context context)
+	throws IOException, InterruptedException
+	{
+        HashMap<Text, Integer> hash_map = new HashMap();
+        Integer count;
+        for (Text val : values)
+        {
+            count = hash_map.get(val);
+            if (count == null)
+                hash_map.put(val, new Integer(1));
+            else
+                hash_map.put(val, count + 1);
+        }
+
+        Text max_repeated_word = new Text("");
+        count = 0;
+        for (Map.Entry<Text, Integer> entry : hash_map.entrySet()) {
+            if (entry.getValue() > count) {
+                count = entry.getValue();
+                max_repeated_word = entry.getKey();
+            }
+        }
+
+        context.write(key, max_repeated_word);
+    }
+}
